a59c312ca3d73f034608a85da4881365191f4710
RomanB <RomanB@RomanB-PC>
2021-01-17 22:26:36 +0200

    start reading and writing XLSX files

---
+++ src/main/java/ua/pp/helperzit/excelParser/ExcelParser.java
@@ -0,0 +1,44 @@
+package ua.pp.helperzit.excelParser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelParser {
+    
+    public static void main(String[] args) throws IOException {
+        
+        File currentDir = new File(".");
+        String path = currentDir.getAbsolutePath();
+        String outFileLocation = path.substring(0, path.length()-1) + "tempWrite.xlsx";
+        String inFileLocation = path.substring(0, path.length()-1) + "tempRead.xlsx";
+        
+        try (Workbook outWorkbook = new XSSFWorkbook()) {
+            Sheet sheet = outWorkbook.createSheet("sheet1");
+            Row row0 = sheet.createRow(0);
+            Cell cell0 = row0.createCell(0);
+            
+            cell0.setCellValue("setted text");
+            
+            FileOutputStream fos = new FileOutputStream(outFileLocation);
+            outWorkbook.write(fos);
+            fos.close();
+        }
+            
+            String parsingResult;
+            try (Workbook inWorkbook = new XSSFWorkbook(new FileInputStream(inFileLocation))) {
+                parsingResult = inWorkbook.getSheetAt(0).getRow(0).getCell(0).getStringCellValue();
+            }
+            System.out.println(parsingResult);
+
+        
+    }
+
+}
