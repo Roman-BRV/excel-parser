9307e4b5a55e4c2664dfa16ba65157f7e7958449
RomanB <RomanB@192.168.129.1>
2021-01-26 07:11:48 +0200

    start unit tests

---
+++ src/test/java/ua/pp/helperzit/excelparser/service/TableGeneratorTest.java
@@ -0,0 +1,19 @@
+package ua.pp.helperzit.excelparser.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TableGeneratorTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+    }
+
+    @Test
+    void testGenerateTable() {
+        fail("Not yet implemented");
+    }
+
+}
